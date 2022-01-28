87d7c3e2533c2fa12e26f77277294235717694bf
KaungMyat <kaungmyat77.km99@gmail.com>
2022-01-29 04:49:04 +0630

    V-0.2

--- src/main/java/com/kmsalt/km_salt_production/main.java
+++ src/main/java/com/kmsalt/km_salt_production/main.java
@@ -11,8 +11,8 @@ public class main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource("mainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("KM Salt Production Dashboard");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/kmsalt/km_salt_production/mainController.java
+++ src/main/java/com/kmsalt/km_salt_production/mainController.java
@@ -1,14 +1,33 @@
 package com.kmsalt.km_salt_production;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
-public class mainController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mainController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Circle proFile;
+    @FXML
+    private Circle proFile1;
+
+    private void proFileLoader(){
+        Image img = new Image("D:\\KM_Salt_Production\\src\\main\\resources\\com\\kmsalt\\km_salt_production\\image\\profile.png");
+        proFile.setFill(new ImagePattern(img));
+    }
+    private void companyFileLoader(){
+        Image img = new Image("D:\\KM_Salt_Production\\src\\main\\resources\\com\\kmsalt\\km_salt_production\\image\\company_logo.png");
+        proFile1.setFill(new ImagePattern(img));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+     proFileLoader();
+     companyFileLoader();
     }
 }
